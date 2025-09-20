ff045eaf34ec23d96d476bbd9a499b12db6e5ed3
100RAB <115261525+100rabx23@users.noreply.github.com>
2025-09-20 20:06:39 +0530

    Add files via upload

---
+++ Compose-app/build/generated/source/buildConfig/debug/com/example/milewebassignment/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.milewebassignment;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.milewebassignment";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String GITLAB_CLIENT_ID = "0466b259b523fcad3c12dc61c6af6f3983b5b26c6174d1cd9b4cdf81a35cba6c";
+  // Field from default config.
+  public static final String GITLAB_CLIENT_SECRET = "gloas-d176c4dc72cbd096fa8bf87d03f71373b8da4960c1f57ae8787072719a82b880";
+}
