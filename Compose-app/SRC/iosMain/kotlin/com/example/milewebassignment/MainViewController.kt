af398827433c60d100aa063d3c58a6fe75d9cfc8
100RAB <115261525+100rabx23@users.noreply.github.com>
2025-11-24 17:24:54 +0530

    Update MainViewController.kt

--- Compose-app/SRC/iosMain/kotlin/com/example/milewebassignment/MainViewController.kt
+++ Compose-app/SRC/iosMain/kotlin/com/example/milewebassignment/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.example.milewebassignment
 
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlin.test.ui
 
 fun MainViewController() = ComposeUIViewController { App() }
